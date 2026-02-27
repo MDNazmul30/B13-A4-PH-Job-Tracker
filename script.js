cf5ccbd17e0e98ba17f37128a2a98f2791c440f3
MD. Nazmul Hasan <md.nazmul.mn989@gmail.com>
2026-02-27 15:49:48 +0600

    add total js functionality

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hellow')
+let total=document.getElementById('total')
+let Rejected=document.getElementById('Rejected')
+let Interview= document.getElementById('Interview')
+
+let allcards=document.getElementById('allcards')
+
+function count(){
+    total.innerText=allcards.children.length;
+}
+count()
