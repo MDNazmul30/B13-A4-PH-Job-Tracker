b51f75187bf5c614c8f32f7fc013716f36495141
MD. Nazmul Hasan <md.nazmul.mn989@gmail.com>
2026-03-03 16:31:35 +0600

    add interview button functionality

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const interviewFilterBtn = document.getElementById('Interview-filter-btn')
 
 let allcards=document.getElementById('allcards')
 const mainContainer = document.querySelector('main')
+const filterSection = document.getElementById('filterSection')
 
 
 function count(){
@@ -30,8 +31,76 @@ function toggleStyle(id){
     
     console.log(id);
     const selected =document.getElementById(id)
-    console.log(selected);
+    //console.log(selected);
     selected.classList.remove('bg-gray-300','text-black')
     selected.classList.add('bg-black','text-white')
+
+    if(id== 'Interview-filter-btn'){
+        allcards.classList.add('hidden')
+        filterSection.classList.remove('hidden')
+    }
+    else if(id== 'all-filter-btn'){
+        allcards.classList.remove('hidden')
+        filterSection.classList.add('hidden')
+    }
 }
 
+mainContainer.addEventListener('click', function(event){
+   // console.log(event.target.classList.contains('interviewBtn'))
+   if(event.target.classList.contains('interviewBtn')){
+     const parenNode=event.target.parentNode.parentNode;
+    const heading1st =parenNode.querySelector('.heading1st').innerText
+    const heading2nd =parenNode.querySelector('.heading2nd').innerText
+    const descriptiion=parenNode.querySelector('.descriptiion').innerText
+    const salaryDetails=parenNode.querySelector('.salaryDetails').innerText
+    parenNode.querySelector('.status').innerText='Interview'
+    const cardInfo ={
+        salaryDetails,
+        descriptiion,
+        heading2nd,
+        heading1st
+
+    }
+   const jobexist= InterviewList.find(item=>item.heading1st == cardInfo.heading1st)
+   
+   if(!jobexist){
+    InterviewList.push(cardInfo)
+   }
+   filterInterview()
+   count()
+
+   }
+   
+})
+
+function filterInterview(){
+  filterSection.innerHTML = ''
+  for(let intervw of InterviewList){
+    console.log(intervw)
+    let div = document.createElement('div');
+    div.className = 'card p-5 border-1 rounded-xl space-y-5'
+   div. innerHTML = `
+                <div class="flex flex-row place-content-between space-y-4">
+                    <div class="">
+                        <h1 class="heading1st">${intervw.heading1st}</h1>
+                        <h1 class="heading2nd">
+                            ${intervw.heading2nd}
+                        </h1>
+                    </div>
+
+                    <i class="fa-solid fa-trash  rounded-full"></i>
+
+
+                </div>
+                <h1 class="salaryDetails">${intervw.salaryDetails}</h1>
+                <button class="status btn btn-sm w-24">Not Applied</button>
+
+                <h1 class="descriptiion">${intervw.descriptiion}</h1>
+                <div class="flex flex-row gap-[8px]">
+                    <button class="btn">Inerview</button>
+                    <button class="btn">Rejected</button>
+                </div>
+`
+filterSection.appendChild(div)
+  }
+}
